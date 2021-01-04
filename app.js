7f7377af07f801825dfc1b3d4dcfb8449bd89da3
Garmhausen <robertgarmhausen@gmail.com>
2021-01-04 13:39:05 -0500

    remove trailing slash

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(expressValidator());
 
 // cors
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://mystery-word-app.web.app/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://mystery-word-app.web.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
